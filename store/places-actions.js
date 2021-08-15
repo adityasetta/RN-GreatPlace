aac802924d51bfe293375baf6601f0c62f186ec9
Aditya Parama Setta <aditya.psetta@bukitmakmur.com>
2021-08-15 19:01:06 +0700

    Save picked image to local storage

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,24 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
-export const addPlace = title => {
-  return { type: ADD_PLACE, placeData: { title: title } };
+export const addPlace = (title, image) => {
+  return async dispatch => {
+    const fileName = image.split('/').pop();
+
+    // documentDirectory will be deleted after uninstall
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
